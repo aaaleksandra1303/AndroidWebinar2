2a1170ae6cf8c839fc9c0af208d5a3bbd6a1566f
Aleksandra Malek <aleksandra1303@gmail.com>
2016-09-10 15:41:33 +0200

    create textView,button and butterknife initialization

--- app/src/main/java/webinar2/powtorka/webinar/akademiakodu/pl/powtorka2web/MainActivity.java
+++ app/src/main/java/webinar2/powtorka/webinar/akademiakodu/pl/powtorka2web/MainActivity.java
@@ -3,19 +3,29 @@ package webinar2.powtorka.webinar.akademiakodu.pl.powtorka2web;
 import android.app.Activity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends Activity {
 
 
+    @BindView(R.id.textView2)
+   public TextView text;
 
-    private TextView text;
+    @BindView(R.id.button)
+    public Button button;
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         //text = (TextView)findViewById(R.id.textView2);
 
