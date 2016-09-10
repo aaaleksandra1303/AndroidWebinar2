7d4dc3ba1eec8915bb75b2b618f8cea7754e7b04
Aleksandra Malek <aleksandra1303@gmail.com>
2016-09-10 15:34:19 +0200

    first commit start project

---
+++ app/src/main/java/webinar2/powtorka/webinar/akademiakodu/pl/powtorka2web/MainActivity.java
@@ -0,0 +1,24 @@
+package webinar2.powtorka.webinar.akademiakodu.pl.powtorka2web;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+
+
+    private TextView text;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //text = (TextView)findViewById(R.id.textView2);
+
+
+    }
+}
