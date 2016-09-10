7b00b298d9b791c5dfaf99684ed2317e82f7b6cd
Aleksandra Malek <aleksandra1303@gmail.com>
2016-09-10 16:13:38 +0200

    sleep progres bar and surround trycatch

--- app/src/main/java/webinar2/powtorka/webinar/akademiakodu/pl/powtorka2web/MainActivity.java
+++ app/src/main/java/webinar2/powtorka/webinar/akademiakodu/pl/powtorka2web/MainActivity.java
@@ -58,8 +58,13 @@ public class MainActivity extends Activity {
             for(int i=0;i<=20;i++)
             {
                 publishProgress(i);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-                
+
             return null;
 
         }
